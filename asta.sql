2ed94b5a3fa4a34748306f7cafb0c6d588b63eda
eduardVerse <107571763+eduardVerse@users.noreply.github.com>
2023-03-16 15:38:11 +0100

    Add files via upload

---
+++ asta.sql
@@ -0,0 +1,46 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE DATABASE IF NOT EXISTS `asta`;
+USE `asta`;
+
+-- tabella categorie
+
+CREATE TABLE `categorie` (
+  `CategoriaID` int(1) NOT NULL,
+  `NomeCategoria` varchar(9) DEFAULT NULL,
+  PRIMARY KEY (`CategoriaID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- tabella oggetti
+
+CREATE TABLE `oggetti` (
+  `OggettoID` int(2) NOT NULL,
+  `NomeOggetto` varchar(9) DEFAULT NULL,
+  `CategoriaID` varchar(9) DEFAULT NULL,
+  `Quantita` int(1) DEFAULT NULL,
+  `BaseAsta` float(1) DEFAULT NULL,
+  `IpMulticast` varchar(9) DEFAULT NULL,
+  PRIMARY KEY (`StudioID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `oggetti`
+  ADD CONSTRAINT `FK1_oggetti` FOREIGN KEY (`CategoriaID`) REFERENCES `categorie` (`CategoriaID`);
+
+-- tabella clienti
+
+CREATE TABLE `clienti` (
+  `ClienteID` int(2) NOT NULL,
+  `Nome` varchar(9) DEFAULT NULL,
+  `Cognome` varchar(9) DEFAULT NULL,
+  `NumeroTelefonico` varchar(9) DEFAULT NULL,
+  `OggettoID` int(1) NOT NULL,
+  PRIMARY KEY (`ClienteID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `clienti`
+  ADD CONSTRAINT `FK1_clienti` FOREIGN KEY (`OggettoID`) REFERENCES `oggetti` (`OggettoID`);
+
+COMMIT;
